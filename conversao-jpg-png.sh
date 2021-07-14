ff2ab4f7562f2cabc480e5be6208df2a21529703
gabrielnascimentosilva <gabriel.nascimento@soitic.com>
2021-07-14 15:20:14 -0300

    automatizando convertendo todas as imagens e corrigindo o nome.png

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CAMINHO=~/Documentos/GitHubPessoal/Shellscript/imagens
 <<COMENTARIO
+CAMINHO=~/Documentos/GitHubPessoal/Shellscript/imagens
+
 for imagem in $@
 do 
     convert $CAMINHO/$imagem.jpg $CAMINHO/$imagem.png
@@ -13,5 +14,16 @@ COMENTARIO
 cd ~/Documentos/GitHubPessoal/Shellscript/imagens
 for imagem in *.jpg
 do 
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
+    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+done
+<<COMENTARIO1
+cd ~/Documentos/GitHubPessoal/Shellscript/imagens
+for imagem in *.jpg
+do 
     convert $imagem $imagem.png
 done
+DESSA MANIERA FICA EXTENSOES NO NOME DA IMAGEM, DUAS VEZES 
+DEVERÁ SER USADO O COMANDO AWK
+ls | awk -F. '{print $1 }'
+COMENTARIO1
