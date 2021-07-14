d973c1ffa1ab26bdf5019668377896f795acc234
gabrielnascimentosilva <gabriel.nascimento@soitic.com>
2021-07-14 15:08:39 -0300

    automatizando pra reconhecer todas as imagens jpg e converter em png sem passar parametros manual

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 CAMINHO=~/Documentos/GitHubPessoal/Shellscript/imagens
-
+<<COMENTARIO
 for imagem in $@
 do 
     convert $CAMINHO/$imagem.jpg $CAMINHO/$imagem.png
+done
+
+$@ pega todos parametros passados pelo usuario na execucao do script
+COMENTARIO
+
+cd ~/Documentos/GitHubPessoal/Shellscript/imagens
+for imagem in *.jpg
+do 
+    convert $imagem $imagem.png
 done
