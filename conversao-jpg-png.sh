5140e39e0f6ca0bc4d9bd0cb4b0f80bc4cd995a9
gabrielnascimentosilva <gabriel.nascimento@soitic.com>
2021-07-14 14:17:46 -0300

    primeiro script de conversao de imagens simples com passagem de parametro unica vez

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-convert ~/Documentos/GitHubPessoal/Shellscript/imagens/cardapioteste.jpg ~/Documentos/GitHubPessoal/Shellscript/imagens/cardapioteste.png
+convert ~/Documentos/GitHubPessoal/Shellscript/imagens/$1.jpg ~/Documentos/GitHubPessoal/Shellscript/imagens/$1.png
 
